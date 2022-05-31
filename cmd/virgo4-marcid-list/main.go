33597cc7c1a5c5a2696d0bc07820d9a3f462fb1c
Dave Goldstein <dpg3k@virginia.edu>
2022-05-31 12:56:27 -0400

    Move to the uva-aws-s3 helper module

--- cmd/virgo4-marcid-list/main.go
+++ cmd/virgo4-marcid-list/main.go
@@ -3,11 +3,16 @@ package main
 import (
 	"fmt"
 	"io"
+	"io/ioutil"
 	"log"
 	"os"
+	"strings"
+
+	"github.com/uvalib/uva-aws-s3-sdk/uva-s3"
 )
 
 var downloadDir = "/tmp"
+var ErrBadName = fmt.Errorf("bad file name")
 
 //
 // main entry point
@@ -18,8 +23,27 @@ func main() {
 	localName := cfg.InFileName
 
 	var err error
+
 	if cfg.MustDownload == true {
-		localName, err = s3download(downloadDir, cfg.InFileName)
+
+		// load our AWS s3 helper object
+		s3Svc, err := uva_s3.NewUvaS3(uva_s3.UvaS3Config{Logging: true})
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		// create temp file
+		tmp, err := ioutil.TempFile(downloadDir, "")
+		if err != nil {
+			log.Fatal(err)
+		}
+		tmp.Close()
+		localName = tmp.Name()
+
+		// download the file
+		bucket, key := s3Split(cfg.InFileName)
+		o := uva_s3.NewUvaS3Object(bucket, key)
+		err = s3Svc.GetToFile(o, localName)
 		if err != nil {
 			log.Fatal(err)
 		}
@@ -58,6 +82,22 @@ func main() {
 	if cfg.MustDownload == true {
 		_ = os.Remove(localName)
 	}
+}
+
+func s3Split(s3Name string) (string, string) {
+
+	// remove the prefix
+	str := strings.Replace(s3Name, "s3://", "", 1)
+
+	// split the rest
+	tokens := strings.Split(str, "/")
+	if len(tokens) < 2 {
+		log.Fatal(ErrBadName)
+	}
+
+	bucket := tokens[0]
+	key := strings.Replace(str, fmt.Sprintf("%s/", bucket), "", 1)
+	return bucket, key
 }
 
 //
--- go.mod
+++ go.mod
@@ -3,7 +3,8 @@ module github.com/uvalib/virgo4-marcid-list
 go 1.13
 
 require (
-	github.com/aws/aws-sdk-go v1.42.17
+	github.com/aws/aws-sdk-go v1.44.24 // indirect
 	github.com/kr/pretty v0.1.0 // indirect
+	github.com/uvalib/uva-aws-s3-sdk/uva-s3 v0.0.0-20220527134326-5f592a4b5fe9
 	gopkg.in/check.v1 v1.0.0-20180628173108-788fd7840127 // indirect
 )
